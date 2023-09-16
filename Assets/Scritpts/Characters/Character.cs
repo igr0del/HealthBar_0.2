1d6c8c36be7966855653503b69724190d9389ba6
IGRODELOFF <inshildr@gmail.com>
2023-09-16 08:55:59 +0300

    0.3

--- Assets/Scritpts/Characters/Character.cs
+++ Assets/Scritpts/Characters/Character.cs
@@ -1,7 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class Character : MonoBehaviour
 {
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+    [SerializeField] private float _maxHealth;
+    [SerializeField] private float _takeDamageButton;
+    [SerializeField] private float _takeHealthButton;
+    [SerializeField] private float _speedColorChange;
+
+    private float _correctHealth;
+
+    public float MaxHealth => _maxHealth;
+    public float CorrectHealth => _correctHealth;
+
+    private void Start()
+    {
+        _correctHealth = _maxHealth;
+
+        SetValueDamageButton(_takeDamageButton);
+        SetValueHealthButton(_takeHealthButton);
+    }
+
+    private void SetValueDamageButton(float damage)
+    {
+        _takeDamageButton = damage;
+    }
+
+    private void SetValueHealthButton(float health)
+    {
+        _takeHealthButton = health;
+    }
+
+    public void TakeDamage()
+    {
+        _spriteRenderer.DOColor(Color.white, 0).SetLoops(2, LoopType.Yoyo);
+
+        if (_correctHealth > 0)
+            _correctHealth -= _takeDamageButton;
+        else
+            _correctHealth = 0;
+
+        _spriteRenderer.DOColor(Color.red, _speedColorChange).SetLoops(2, LoopType.Yoyo);
+    }
+
+    public void TakeHealth()
+    {
+        _spriteRenderer.DOColor(Color.white, 0).SetLoops(2, LoopType.Yoyo);
+
+        if (_correctHealth < _maxHealth)
+            _correctHealth += _takeHealthButton;
+        else if (_correctHealth > _maxHealth)
+            _correctHealth = _maxHealth;
+
+        _spriteRenderer.DOColor(Color.green, _speedColorChange).SetLoops(2, LoopType.Yoyo);
+    }
 }
--- Assets/Scritpts/HUD/HealthBar.cs
+++ Assets/Scritpts/HUD/HealthBar.cs
@@ -1,10 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    
+    [SerializeField] private Slider _healthSlider;
+    [SerializeField] private Slider _easeHealthSlider;
+    [SerializeField] private Character _character;
+    [SerializeField] private float _lerpSpeed = 0.05f;
+
+    private void Start()
+    {
+        _character = GetComponent<Character>();
+
+        SetMaxHealth(_character.MaxHealth);
+    }
+
+    private void Update()
+    {
+        SetCorrectHealth(_character.CorrectHealth);
+    }
+
+    private void SetMaxHealth(float health)
+    {
+        _healthSlider.maxValue = health;
+        _easeHealthSlider.maxValue = health;
+
+        _healthSlider.value = health;
+        _easeHealthSlider.value = health;
+    }
+
+    private void SetCorrectHealth(float health)
+    {
+        if (_character.CorrectHealth <= _healthSlider.value)
+        {
+            _healthSlider.value = health;
+            _easeHealthSlider.value = Mathf.Lerp(_easeHealthSlider.value, health, _lerpSpeed);
+        }
+        else 
+        {
+            _easeHealthSlider.value = health;
+            _healthSlider.value = Mathf.Lerp(_healthSlider.value, health, _lerpSpeed);
+        }
+    }
 }
